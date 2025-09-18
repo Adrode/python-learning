cfb2e5a8ad00dd4d8d1086e74e828a3b29443c43
Adrian <adrianwozniak20@gmail.com>
2025-09-18 13:32:16 +0200

    Hello world

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-print("Hello world!")
+name = input("What's your name?: ")
+age = input("How old are you?: ")
+
+print("Hello " + name + "!")
+if int(age) >= 18:
+    print("You're an adult")
+elif int(age) < 18:
+    print("You're a kid")
+else:
+    print("Something went wrong!")
