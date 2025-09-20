07df1f80f90eb648b42f2a6d2b2afd18e2ea04fc
Adrian <adrianwozniak20@gmail.com>
2025-09-20 12:07:12 +0200

    boolean, comparison, logical operators

---
+++ logical_operators.py
@@ -0,0 +1,14 @@
+# isDark = True
+# isBright = False
+
+# print(isDark and isBright)
+# print(isDark or isBright)
+
+a = 5
+b = 6
+
+# print(a >= 5 and a < b)
+# print(a > b or b > 6 or a > 4)
+
+c = 5
+print(not c % 2 == 0)
