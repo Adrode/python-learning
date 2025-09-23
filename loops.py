ec09bde3efd68c4bb31f1327487eeb8c17bc24d5
Adrian <adrianwozniak20@gmail.com>
2025-09-23 12:25:14 +0200

    loops: continue, break

--- loops.py
+++ loops.py
@@ -1,11 +1,12 @@
-# import time
+import time
 
-# i = 1
+i = 1
 
-# while i < 10:
-#   print(i)
-#   i += 1
-#   time.sleep(1)
+while i < 10:
+  print(i)
+  i += 1
+  time.sleep(1)
+# ------------------------
 
 for i in range(0, 4):
   print(i)
@@ -13,3 +14,15 @@ for i in "JakiS_StriNGG":
   print(i)
 for _ in range(0, 2):
   print("Hello world!")
+# ------------------------
+
+for i in range(0, 5):
+  if i == 2:
+    continue
+  print(i)
+# ------------------------
+
+for i in range(0, 5):
+  if i == 2:
+    break
+  print(i)
