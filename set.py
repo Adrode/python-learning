1e05ae3e3d4367aabfeb67cd6c1716a30af090b2
Adrian <adrianwozniak20@gmail.com>
2025-09-29 17:06:47 +0200

    set

---
+++ set.py
@@ -0,0 +1,28 @@
+names = {"jabłko", "gruszka", "banan", "ogórek"}
+names2 = {"pomidor", "ogórek", "rzodkiewka"}
+
+
+# names.remove("jabłko")
+# print(names)
+# names.update({"jabłko", "jabłko"})
+# print(names)
+# names.add("cytryna")
+# print(names)
+# names.update("kiwi")
+# print(names)
+
+# union
+# print(names | names2)
+# print(names.union(names2))
+
+# intersection
+# print(names & names2)
+# print(names.intersection(names2))
+
+# difference
+# print(names - names2)
+# print(names2.difference(names))
+
+# symmetric difference
+# print(names ^ names2)
+# print(names.symmetric_difference(names2))
