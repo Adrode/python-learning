01ed7fec48942bc1cf0dfc3616b8e3ef38f2efcb
Adrian <adrianwozniak20@gmail.com>
2025-11-11 13:25:37 +0100

    insertion sort

---
+++ DSA/sorting_algorithms/insertion_sort.py
@@ -0,0 +1,17 @@
+# Insertion sort
+
+A = [-10, 1, 15, 3, 2, 5, 6, -1, 3]
+
+def insertion_sort(arr):
+  n = len(arr)
+  for i in range(1, n):
+    print(f"{i}: ")
+    for j in range(i, 0, -1):
+      if arr[j] < arr[j-1]:
+        arr[j], arr[j-1] = arr[j-1], arr[j]
+        print(f"{i}.{j}: {arr}")
+      else:
+        break
+
+insertion_sort(A)
+print(A)
