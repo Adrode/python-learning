3c4828981c98fd3fc61177ffe4384fc90138086f
Adrian <adrianwozniak20@gmail.com>
2025-11-11 13:01:01 +0100

    bubble sort

--- DSA/sorting_algorithms/bubble_sort.py
+++ DSA/sorting_algorithms/bubble_sort.py
@@ -1,1 +1,18 @@
 # Bubble Sort
+#   Time O(n^2)
+#   Space O(1)
+
+A = [9, 1, 15, 3, 2, 5, 6, -1, 3]
+
+def bubble_sort(arr):
+  n = len(arr)
+  check = True
+  while check:
+    check = False
+    for i in range(1, n):
+      if arr[i] < arr[i-1]:
+        check = True
+        arr[i-1], arr[i] = arr[i], arr[i-1]
+
+bubble_sort(A)
+print(A)
