e63a811cf357d3971eaa6a5e8de7925f893d4e40
Adrian <adrianwozniak20@gmail.com>
2025-11-19 18:06:29 +0100

    regular expressions

---
+++ basics/regex.py
@@ -0,0 +1,26 @@
+import re
+
+text = input("Type text: ")
+
+# email
+email_pattern = r"^[a-zA-Z0-9.-_]+@[a-zA-Z]+\.(com|net|org|edu)$"
+
+if re.search(email_pattern, text):
+  print("There is a valid email")
+else:
+  print("There is no email")
+
+# only digits
+only_digits = r"^[0-9]+$"
+
+if re.search(only_digits, text):
+  print("There is an only-digits")
+else:
+  print("There is no only-digits")
+
+# fullname
+full_name = r"^[A-Z][a-z]+ [A-Z][a-z]+$"
+if re.search(full_name, text):
+  print("There is a fullname")
+else:
+  print("There is no fullname")
