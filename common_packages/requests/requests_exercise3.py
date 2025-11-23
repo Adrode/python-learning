5a44d70c2f5bb4c54ced3a9e554500ad59a84240
Adrian <adrianwozniak20@gmail.com>
2025-11-23 13:47:23 +0100

    requests get exercise

--- common_packages/requests/requests_exercise3.py
+++ common_packages/requests/requests_exercise3.py
@@ -1,17 +1,23 @@
 import requests
 
-pokemon_name = input("Type pokemon name: ")
-response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon_name}")
+def get_pokemon_info():
+  while True:
+    pokemon_name = input("Type pokemon name: ")
+    response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon_name}")
+    if response.status_code != 200:
+      print("This pokemon does not exist. Type again.")
+      continue
+    else:
+      pokemon_data = response.json()
+      print(f"Name: {pokemon_data['name']}")
+      print(f"Base XP: {pokemon_data['base_experience']}")
+      print(f"Height: {pokemon_data['height']}")
+      print(f"Weight: {pokemon_data['weight']}")
+      pokemon_types = [item['type']['name'] for item in pokemon_data['types']]
+      print(f"Types: {", ".join(pokemon_types)}")
+      print("Stats:")
+      for item in pokemon_data['stats']:
+        print(f" {item['stat']['name'].capitalize()}: {item['base_stat']}")
+      break
 
-def get_pokemon_moves(r, name):
-  if r.status_code != 200:
-    print(f"No response. Code: {r.status_code}")
-  else:
-    pokemon_data = r.json()
-    pokemon_moves = [item['move']['name'] for item in pokemon_data['moves']]
-    pokemon_moves.sort()
-    print(f"{name} has {len(pokemon_moves)} moves:")
-    for item in pokemon_moves:
-      print(f"- {item}")
-  
-get_pokemon_moves(response, pokemon_name)
+get_pokemon_info()
